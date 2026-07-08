720750aca2cebb1fe5c0cf9d874c99ab39b91c36
AliShmaks <a.shararah@outlook.com>
2026-07-08 11:48:28 +0300

    Delete src directory;

--- src/components/Layout/index.js
+++
@@ -1,5 +0,0 @@
-﻿export { Loader } from './Loader'
-export { ScrollProgress } from './ScrollProgress'
-export { Toast, showToast } from './Toast'
-export { Navbar } from './Navbar'
-export { Footer } from './Footer'
